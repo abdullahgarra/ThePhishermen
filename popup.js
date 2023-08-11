7a5254b30b409674181f93b6a1409171f0aaddf1
hilalewin <hilalewin98@gmail.com>
2023-08-11 21:06:16 +0300

    Added tooltips, need to fix their appearances in div

--- popup.js
+++ popup.js
@@ -11,35 +11,47 @@ document.addEventListener('DOMContentLoaded', function() {
         if (message.includes("fts") && !(message.includes("ftsd"))) {
             document.getElementById('message-text1').innerHTML =`
             <span><img src="icons/fts1.svg" class="bullet-icon" alt="Bullet Icon"></span>
-            <span>This is the first time you have received</span>
-            <br>
-            <span>an email from this sender.</span>
+            <span class="tooltip">
+              <span>This is the first time you have received</span>
+              <br>
+              <span>an email from this sender.
+              <span class="tooltiptext"> To detect this sender as a new sender again, erase this email after reading it.   </span>
+              </span>
           `;
         }
         else if (message.includes("ftsd")) {
           document.getElementById('message-text1').innerHTML =`
           <span><img src="icons/ftsd.svg" class="bullet-icon" alt="Bullet Icon"></span>
-          <span>This is the first time you have received</span>
-          <br>
-          <span>an email from this domain.</span>
+          <span class="tooltip">
+            <span>This is the first time you have received</span>
+            <br>
+            <span>an email from this domain.
+            <span class="tooltiptext"> To detect this domain as a new domain again, erase this email after reading it.   </span>
+            </span>
         `;
         }
         else document.getElementById('message-text1').style.display = "none"
         if (message.includes("pl")){
             document.getElementById('message-text2').innerHTML =`
             <span><img src="icons/pl.svg" class="bullet-icon" alt="Bullet Icon"></span>
-            <span>This email contains links that are</span>
-            <br>
-            <span>identified as phishing.</span>
+            <span class="tooltip">
+              <span>This email contains links that are</span>
+              <br>
+              <span>identified as phishing.
+              <span class="tooltiptext"> Be carful when clicking on links from this email.</span>
+              </span>
           `;
         }
         else document.getElementById('message-text2').style.display = "none"
         if (message.includes("pc")){
             document.getElementById('message-text3').innerHTML =`
             <span><img src="icons/pc.svg" class="bullet-icon" alt="Bullet Icon"></span>
-            <span>The content of this email raises</span>
-            <br>
-            <span>suspicion of phishing.</span>
+            <span class="tooltip">
+              <span>The content of this email raises</span>
+              <br>
+              <span>suspicion of phishing.
+              <span class="tooltiptext"> Be carful when replaying to this email.</span>
+              </span>
           `;
         }
         else document.getElementById('message-text3').style.display = "none"        
