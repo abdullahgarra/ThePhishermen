8b5491b02b8465a3e981019140345f0898659de1
hilalewin <hilalewin98@gmail.com>
2023-08-17 00:12:34 +0300

    plaster solution for where the popup is created

--- background.js
+++ background.js
@@ -82,7 +82,7 @@ function setStoredAccessToken(token, callback) {
 
 function preferencesPopUp(preferences){
   var popupUrl = chrome.runtime.getURL("preferencesPopUp.html") + `?message=${encodeURIComponent(preferences)}`;
-  chrome.windows.create({ url: popupUrl, type: "popup", width: 400, height: 550});
+  chrome.windows.create({ url: popupUrl, type: "popup", width: 400, height: 550, left: 400});
 }
 
 
@@ -195,7 +195,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (message.includes("ftsd") ||message.includes("fts") || message.includes("pl")
         || message.includes("bg") || message.includes("cdg") || message.includes("u")){
     var popupUrl = chrome.runtime.getURL("popup.html") + `?message=${encodeURIComponent(message)}`;
-    chrome.windows.create({ url: popupUrl, type: "popup", width: 400, height: 400 });
+    chrome.windows.create({ url: popupUrl, type: "popup", width: 400, height: 400, left: 400 });
     sendResponse({ message: "Popup created!" });
     }
     //chrome.tabs.create({ url: `popup.html?message=${encodeURIComponent(message)}` });
