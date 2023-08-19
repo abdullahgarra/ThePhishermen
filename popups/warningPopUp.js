57990fc8cd6939f9d4f6ad3141459113768125c1
hilalewin <hilalewin98@gmail.com>
2023-08-19 12:08:31 +0300

    Started working on popupwarning

--- popups/warningPopUp.js
+++ popups/warningPopUp.js
@@ -3,134 +3,116 @@ document.addEventListener('DOMContentLoaded', function() {
     const urlParams = new URLSearchParams(window.location.search);
     const message = urlParams.get('message');
 
-    const alerts = ["ftsd", "fts", "pl", "bg", "cdg", "u"];
-
-
-    const includesAlert = alerts.some(alert => message.includes(alert));
-
-    // Apply different background styles based on the message
-    // Every finding will cause the alert, beside "phishing content"
-    if (includesAlert)
-      {
-        document.body.classList.add('warning');
-        document.getElementById('message-heading').textContent = 'Warning!';
-        document.getElementById('message-image').src = 'imgs/warning-icon.svg';
-
-        // First time sender, not first time domain
-        if (message.includes("fts") && !(message.includes("ftsd"))) {
-            document.getElementById('message-text1').innerHTML =`
-            <span class="fts-trigger">
-              <a href="#" id="fts-link">
-                <img src="imgs/fts1.svg" class="bullet-icon" alt="Bullet Icon">
-              </a>
-            </span>
-            <span>This is the first time you received</span>
-            <br>
-            <span>an email from this sender.</span>
-          `;
-          createListener("fts-trigger", "Notes On First Time Sender", "Notes On First Time Sender");
-
-        }
-        // First time domain (sender also)
-        else if (message.includes("ftsd")) {
-          document.getElementById('message-text1').innerHTML =`
-          <span class="ftsd-trigger">
-            <a href="#" id="ftsd-link">
-              <img src="imgs/ftsd.svg" class="bullet-icon" alt="Bullet Icon">
-            </a>
-          </span>
-          <span>This is the first time you received</span>
-          <br>
-          <span>an email from this domain.</span>
-        `;
-        createListener("ftsd-trigger", "Notes On First Time Domain", "Notes On First Time Domain");
-
-        }
-        else document.getElementById('message-text1').style.display = "none"
-        
-        // Phishing links
-        if (message.includes("pl")){
-            document.getElementById('message-text2').innerHTML =`
-            <span class="links-trigger">
-              <a href="#" id="links-link">
-                <img src="imgs/pl.svg" class="bullet-icon" alt="Bullet Icon">
-              </a>
-            </span>
-            <span>This email contains links that are</span>
-            <br>
-            <span>identified as phishing. </span>
-          `;
-          createListener("links-trigger", "Notes On Suspicious Links", "Suspicious links phishy");
-
-        }
-        else document.getElementById('message-text2').style.display = "none"
-        
-        // Phishing content
-        if (message.includes("pc")){
-            document.getElementById('message-text3').innerHTML =`
-            <span class="content-trigger">
-              <a href="#" id="content-link">
-                <img src="imgs/pc.svg" class="bullet-icon" alt="Bullet Icon">
-              </a>
-            </span>
-            <span>The content of this email raises</span>
-            <br>
-            <span>suspicion of phishing.</span>
-          `;
-          createListener("content-trigger", "Notes On Suspicious Content", "Suspicious Suspicious Suspicious");
-        }
-
-        else document.getElementById('message-text3').style.display = "none"  
-        
-        // Bad Grammar/spelling 
-        if (message.includes("bg")){
-          document.getElementById('message-text4').innerHTML =`
-          <span class="grammar-trigger">
-            <a href="#" id="grammar-link">
-              <img src="imgs/g.png" class="bullet-icon" alt="Bullet Icon">
-            </a>
-          </span>
-          <span>The content of this email contains</span>
-          <br>
-          <span> several instances of poor grammar.</span>
-        `;
-        createListener("grammar-trigger", "Notes On Bad Grammar", "Shila doesn't know Grammar");
-        }
-        // Can't calculate Grammar/spelling 
-        else if (message.includes("cdg")){
-          document.getElementById('message-text4').innerHTML =`
-          <span>
-              <img src="imgs/g.png" class="bullet-icon" alt="Bullet Icon">
-          </span>
-          <span>Can't determine if the content of</span>
-          <br>
-          <span>this email has bad grammar.</span>
-        `;
-        }
-        else document.getElementById('message-text4').style.display = "none"  
-        
-        // Urgency sense 
-        if (message.includes("u")){
-          document.getElementById('message-text5').innerHTML =`
-          <span class="urgent-trigger">
-            <a href="#" id="urgent-link">
-              <img src="imgs/u.jpg" class="bullet-icon" alt="Bullet Icon">
-            </a>
-          </span>
-          <span>The content of this email raises</span>
-          <br>
-          <span>sense of urgency.</span>
-        `;
-        createListener("urgent-trigger", "Notes On Urgent Sense", "Shila this is urgent");
+    const alerts = {
+      'ftsd': {
+        title: 'Notes On First Time Domain',
+        text: 'This is the first time you received an email from this domain.',
+        imgSrc: 'imgs/ftsd.svg',
+        p: 'message-fts'
+      },
+      'fts': {
+        title: 'Notes On First Time Sender',
+        text: 'This is the first time you received an email from this sender.',
+        imgSrc: 'imgs/fts.svg',
+        p: 'message-fts'
+      },
+      'pl': {
+        title: 'Notes On Suspicious Links',
+        text: 'This email contains links that are identified as phishing.',
+        imgSrc: 'imgs/pl.svg',
+        p: 'message-links'
+      },
+      'pc': {
+        title: 'Notes On Suspicious Content',
+        text: 'The content of this email raises suspicion of phishing.',
+        imgSrc: 'imgs/pc.svg',
+        p: 'message-content'
+      },
+      'bg': {
+        title: 'Notes On Bad Grammar',
+        text: 'The content of this email contains several instances of poor grammar.',
+        imgSrc: 'imgs/g.png',
+        p: 'message-grammar'
+      },
+      'cdg': {
+        title: '',
+        text: "Can't determine if the content of this email has bad grammar.",
+        imgSrc: 'imgs/g.png',
+        p: 'message-grammar'
+      },
+      'u': {
+        title: 'Notes On Urgent Sense',
+        text: 'The content of this email raises a sense of urgency.',
+        imgSrc: 'imgs/u.jpg',
+        p: 'message-urgent'
       }
-      else document.getElementById('message-text5').style.display = "none"       
-      }
+    };
+
+    applyStylesAndListeners(message, alerts);
 
     var exitButton = document.getElementById('exitButton');
-    exitButton.addEventListener('click', function() {
-      window.close();
-    });
+        exitButton.addEventListener('click', function() {
+          window.close();
+    });        
   });
+
+  function applyStylesAndListeners(message, alerts) {
+    const validAlerts = Object.keys(alerts).filter(alert => message.includes(alert));
+  
+    if (validAlerts.length === 0 || message.includes('pc')) {
+      return;
+    }
+  
+    const body = document.body;
+    body.classList.add('warning');
+    const heading = document.getElementById('message-heading');
+    heading.textContent = 'Warning!';
+    const image = document.getElementById('message-image');
+    image.src = 'imgs/warning-icon.svg';
+  
+    const textElements = {
+      'message-text1': validAlerts.includes('ftsd') ? alerts['ftsd'].text : '',
+      'message-text2': validAlerts.includes('pl') ? alerts['pl'].text : '',
+      'message-text3': validAlerts.includes('pc') ? alerts['pc'].text : '',
+      'message-text4': validAlerts.includes('bg') ? alerts['bg'].text : alerts['cdg'].text,
+      'message-text5': validAlerts.includes('u') ? alerts['u'].text : ''
+    };
+  
+    for (const elementId in textElements) {
+      const element = document.getElementById(elementId);
+      element.innerHTML = textElements[elementId];
+      if (textElements[elementId] === '') {
+        element.style.display = 'none';
+      }
+    }
+  
+    for (const validAlert of validAlerts) {
+      const alertSpan = document.createElement('span');
+      alertSpan.className = `${validAlert}-trigger`;      
+      const alertA = document.createElement('a');
+      alertA.href = '#';
+      alertA.id = `${validAlert}-link`;
+      alertSpan.appendChild(alertA);
+      const imgElement = document.createElement('img');
+      imgElement.src = alerts[validAlert].imgSrc;
+      // alert(alerts[validAlert]);
+      imgElement.className = 'bullet-icon';
+      imgElement.alt = 'Bullet Icon';
+      alertA.appendChild(imgElement);
+      alert(imgElement.src);
+      
+      const messageText1 = document.getElementById('message-text1');
+
+      messageText1.appendChild(alertSpan);
+
+      // Append the <a> element within the existing HTML structure
+
+
+      // Listener for more information
+      createListener(`${validAlert}-trigger`, alerts[validAlert].title, alerts[validAlert].text);
+    }
+  }
+
 
 function createListener(span_name, title, text){
   const popupTriggers = document.querySelectorAll('.' + span_name);
