c518b800ba7932fa22366605b2e5ee0fd571b70c
hilalewin <hilalewin98@gmail.com>
2023-09-02 23:26:23 +0300

    Serious fix of app format

--- app.py
+++ app.py
@@ -16,37 +16,7 @@ import warnings
 warnings.filterwarnings('ignore')
 
 # Tokenize and preprocess the sentences
-nltk.download("punkt")
-nltk.download("stopwords")
 
-stop_words = set(stopwords.words("english"))
-
-pipe = pipeline(model="facebook/bart-large-mnli")
-
-model_path = 'ml_models/svm_model.pkl'
-# Load the model from the pickle file
-with open(model_path, 'rb') as file:
-    loaded_model_info = joblib.load(model_path)
-    # Load the model from the pickle file
-    loaded_svm = loaded_model_info['svm_model']
-    vectorizer = loaded_model_info['vectorizer']
-    best_threshold = loaded_model_info['best_threshold']
-    beta = loaded_model_info['beta']
-file.close()
-
-model_path = 'ml_models/gradient_boosting_model.pkl'
-reduced_model_path = 'ml_models/reduce_gradient_boosting_model.pkl'
-
-# Load the model from the pickle file
-with open(model_path, 'rb') as file:
-    gbc = pickle.load(file)
-file.close()
-with open(reduced_model_path, 'rb') as file:
-    rgbc = pickle.load(file)
-file.close()
-
-happy_tt = HappyTextToText("T5", "vennify/t5-base-grammar-correction")
-args = TTSettings(num_beams=5, min_length=1)
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -61,156 +31,213 @@ cors = CORS(app)
 def analyze():
     email_obj = Email.from_json(request.get_data())
 
-    # Calculate the phishing prob based on the content
-    msg = create_analyze_phishing(email_obj.preferences, email_obj.decoded_content, email_obj.counter_from_sender,
-                                  email_obj.counter_from_domain, email_obj.links)
-    analysis_result = {'Answer': msg}
+    # Calculate the phishing probability
+    msg = create_analyze_phishing(email_obj.preferences, email_obj.decoded_content,
+                                  email_obj.indicator_of_first_time_sender,
+                                  email_obj.indicator_of_first_time_domain, email_obj.links)
 
+    # Results back from create_analyze_phishing
+    analysis_result = {'Answer': msg}
     return jsonify(analysis_result)
 
 
-def analyze_phishing_content(content):
-    # preprocessed_content = preprocess(content)  # Preprocess the question using your preprocessing steps
-    vectorized_content = vectorizer.transform([content])  # Transform the question into a numerical representation
-
-    y_scores = loaded_svm.predict_proba(vectorized_content)[:, 1]
-
-    # Apply the optimized threshold to make predictions
-    y_pred = int(y_scores >= best_threshold)
-    return y_pred
-
-
-def analyze_phishing_links(links):
-    bad_links = []
-    for link in links:
-        obj = FeatureExtraction(link)
-        x = np.array(obj.getFeaturesList()).reshape(1, 14)
-        y_pred = gbc.predict(x)[0]
-        # 1 is phishing, 0 is non phishing
-        if y_pred == 1:
-            bad_links.append(link)
-    return bad_links
-
-
-def reduced_analyze_phishing_links(links):
-    bad_links = []
-    for link in links:
-        obj = ReducedFeatureExtraction(link)
-        x = np.array(obj.getFeaturesList()).reshape(1, 5)
-        y_pred = rgbc.predict(x)[0]
-        # 1 is phishing, 0 is non phishing
-        if y_pred == 1:
-            bad_links.append(link)
-    return bad_links
-
-
 # Analyze the probability to be phishing
-def create_analyze_phishing(preferences, content, counter_from_sender, counter_from_domain, links):
-    if "LinksHigh" in preferences:
-        bad_links = analyze_phishing_links(links)
-    elif "LinksLow" in preferences:
-        bad_links = reduced_analyze_phishing_links(links)
-    else:
-        bad_links = []
+def create_analyze_phishing(preferences, content, indicator_of_first_time_sender,
+                            indicator_of_first_time_domain, links):
 
+    res = []
+
+    # Domain case contains sender case
+    # Indicator can be 1 or 0
+    if indicator_of_first_time_domain:
+        res.append("ftsd")
+    elif indicator_of_first_time_sender:
+        res.append("fts")
+
+    # If the content of the email is empty
+    if len(content) <= 2:
+        return res
+
+    # --------------- Content --------------- #
     if "Content" in preferences:
         predicted_label_content = analyze_phishing_content(content)
-    else:
-        predicted_label_content = 0
+        if predicted_label_content == 1:
+            res.append("pc")
 
+    # --------------- Links --------------- #
+    links_score = 0
+    if "LinksHigh" in preferences:
+        links_score = analyze_phishing_links(links, "extended")
+    elif "LinksLow" in preferences:
+        links_score = analyze_phishing_links(links, "simpler")
+    if links_score == 1:
+        res.append("pl")
+
+    # ---------- Grammar/Spelling ---------- #
     if "Grammar" in preferences:
         grammar_score = analyze_grammar(content)
-    else:
-        grammar_score = 1.0
-
-    if "Urgency" in preferences:
-        urgency_score = analyze_urgency(content)
-    else:
-        urgency_score = 0.0
-
-    # pc = phishing content
-    # pl = phishing links
-    # fts = first time sender email 
-    # ftsd = first time sender domain 
-    # bg = bad grammar 
-    # cdg  = can't decide grammar
-    # u = urgency
-    res = []
-    # We only care if it is the first time receiving from sender / gotten phishing emails
-    if counter_from_domain:
-        res.append("ftsd")
-    elif counter_from_sender:
-        res.append("fts")
-    if len(bad_links) > 0: res.append("pl")
-    if len(content) > 2 and predicted_label_content == 1: res.append("pc")
-    if len(content) > 2 and grammar_score == -1:
-        res.append('cdg')
-    # Higher grammar_score, means more similar to the valid text
-    # If we are strict, we must allow only high scores
-    elif len(content) > 2:
+        # Can't calculate grammar_score
+        if grammar_score == -1:
+            res.append('cdg')
+            # Higher grammar_score, means more similar to the valid text
         if ("GrammarLow" in preferences) \
                 and (grammar_score < 0.90):
-                res.append('bg')
+            res.append('bg')
         if ("GrammarHigh" in preferences) \
-                and (grammar_score < 0.95): res.append('bg')
-    # Higher urgency_score, means more urgent
-    # If we are strict, we count low scores as urgent too 
-    if len(content) > 2:
+                and (grammar_score < 0.95):
+            res.append('bg')
+
+    # --------------- Urgency --------------- #
+    if "Urgency" in preferences:
+        urgency_score = analyze_urgency(content)
         if ("UrgencyLow" in preferences) \
-                and urgency_score > 0.95: res.append("u")
+                and urgency_score > 0.95:
+            res.append("u")
         if ("UrgencyHigh" in preferences) \
-                and urgency_score > 0.90: res.append("u")
+                and urgency_score > 0.90:
+            res.append("u")
+
     print(res)
     return res
 
 
-# Token indices sequence length is longer than the specified 
-# maximum sequence length for this model (4708 > 512)
-# Therefor need to divide into sentences
-def calculate_words_arr(content):
-    # Replace semicolons with periods
-    # sent_tokenize doesn't split with ;
-    content = content.replace(';', '.')
-    sentences = sent_tokenize(content)
-    words = set()
-    for idx, sentence in enumerate(sentences, start=1):
-        if len(sentence) > 512:
-            return {}
+# ---------------------------- Content ---------------------------- #
+
+content_model_path = 'ml_models/svm_model.pkl'
+# Load the model from the pickle file
+with open(content_model_path, 'rb') as file:
+    loaded_model_info = joblib.load(content_model_path)
+    # Load the model from the pickle file
+    loaded_svm = loaded_model_info['svm_model']
+    vectorizer = loaded_model_info['vectorizer']
+    best_threshold = loaded_model_info['best_threshold']
+    beta = loaded_model_info['beta']
+file.close()
+
+
+def analyze_phishing_content(content):
+    # Transform the content of the email into a numerical representation
+    # based on the model we trained
+    vectorized_content = vectorizer.transform([content])
+
+    # Calculate the probability to pe phishing or not
+    y_scores = loaded_svm.predict_proba(vectorized_content)[:, 1]
+
+    # Apply the optimized threshold to make predictions
+    return int(y_scores >= best_threshold)
+
+
+# ---------------------------- Links ---------------------------- #
+
+extended_model_path = 'ml_models/gradient_boosting_model.pkl'
+reduced_model_path = 'ml_models/reduce_gradient_boosting_model.pkl'
+
+# Load the model from the pickle file
+with open(extended_model_path, 'rb') as file:
+    gbc = pickle.load(file)
+file.close()
+with open(reduced_model_path, 'rb') as file:
+    rgbc = pickle.load(file)
+file.close()
+
+
+# Returns 1 if a phishy link was found
+# Otherwise, 0
+def analyze_phishing_links(links, model_type):
+    for link in links:
+        if model_type == "simpler":
+            obj = ReducedFeatureExtraction(link)
+            x = np.array(obj.getFeaturesList()).reshape(1, 5)
+            y_prediction = rgbc.predict(x)[0]
+        elif model_type == "extended":
+            obj = FeatureExtraction(link)
+            x = np.array(obj.getFeaturesList()).reshape(1, 14)
+            y_prediction = gbc.predict(x)[0]
         else:
-            result = happy_tt.generate_text("grammar: " + sentence, args=args)
-            tmp_words1 = preprocess_sentence(result.text)
-            words.update(tmp_words1)
-    return words
+            raise ValueError("Invalid type")
+        # 1 is phishing, 0 is non phishing
+        if y_prediction == 1:
+            return 1
+    return 0
+
+# ---------------------------- Urgency ---------------------------- #
 
 
-# Done with chatGPT
-# Don't care for commas
-def analyze_grammar(content):
-    # Extract the words
-    preprocessed_words1 = calculate_words_arr(content)
-    if preprocessed_words1 == {}:
-        return -1
-    preprocessed_words2 = preprocess_sentence(content)
-    # Calculate Jaccard similarity, to find similarity
-    intersection_val = len(preprocessed_words1.intersection(preprocessed_words2))
-    union_val = len(preprocessed_words1.union(preprocessed_words2))
-    if union_val == 0:
-        return -1
-    jaccard_similarity = intersection_val / union_val
-    return jaccard_similarity
+pipe = pipeline(model="facebook/bart-large-mnli")
 
 
-def preprocess_sentence(text):
+# Calculate the urgent sense with zero-shot
+def analyze_urgency(content):
+    res = pipe(content,
+               candidate_labels=["urgent"], )
+    return res["scores"][0]
+
+
+# --------------------- Grammar and Spelling --------------------- #
+
+
+nltk.download("punkt")
+nltk.download("stopwords")
+stop_words = set(stopwords.words("english"))
+happy_tt = HappyTextToText("T5", "vennify/t5-base-grammar-correction")
+args = TTSettings(num_beams=5, min_length=1)
+
+
+# Given a text, return the set of words
+# that appeared in the sentence
+# Not including stop words
+def get_set_from_sentence(text):
     words = nltk.word_tokenize(text.lower())
     words = [word for word in words if word.isalnum() and word not in stop_words]
     return set(words)
 
 
-def analyze_urgency(content):
-    res = pipe(content,
-               candidate_labels=["urgent"],
-               )
-    return res["scores"][0]
+# Create a valid version of the content
+# then create the set of words that the version used
+# Had to split into sentences because generate_text
+# is restricted to 512 chars
+def get_set_from_valid_content(content):
+    # Replace semicolons with periods
+    # sent_tokenize doesn't split a sentence because of ;
+    content = content.replace(';', '.')
+    sentences = sent_tokenize(content)
+    words = set()
+
+    # Iterate over sentences
+    for idx, sentence in enumerate(sentences, start=1):
+        if len(sentence) > 512:
+            # Can't call generate_text
+            return {}
+        else:
+            # Generate the valid version of the sentence from the content
+            result = happy_tt.generate_text("grammar: " + sentence, args=args)
+            # Get temporary set of words
+            tmp_set = get_set_from_sentence(result.text)
+            words.update(tmp_set)
+
+    # Returns the combination of all tmp_sets
+    return words
+
+
+# Created with chatGPT
+# Ignores commas
+def analyze_grammar(content):
+    # Extract the words into a set
+    preprocessed_words1 = get_set_from_valid_content(content)
+    preprocessed_words2 = get_set_from_sentence(content)
+
+    if len(preprocessed_words1) == 0 or len(preprocessed_words2) == 0:
+        return -1
+
+    # Calculate Jaccard similarity, to find similarity
+    intersection_val = len(preprocessed_words1.intersection(preprocessed_words2))
+    union_val = len(preprocessed_words1.union(preprocessed_words2))
+
+    # Similarity can't be calculated
+    if union_val == 0:
+        return -1
+
+    return intersection_val / union_val
 
 
 if __name__ == '__main__':
