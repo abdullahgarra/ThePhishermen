92529e524833349c5d9d7863f3dcfb9e38c711b2
hilalewin <hilalewin98@gmail.com>
2023-09-02 23:38:59 +0300

    Some reformating for feature.py

--- feature.py
+++ feature.py
@@ -1,4 +1,3 @@
-
 # Taken from:
 # https://github.com/VaibhavBichave/Phishing-URL-Detection/blob/c25eae02643e2e0c3d221c8950ad8409a13a1b65/feature.py
 
@@ -6,21 +5,20 @@ import ipaddress
 import re
 import urllib.request
 from bs4 import BeautifulSoup
-import socket
 import requests
 from googlesearch import search
 import whois
-from datetime import date, datetime
-import time
-from dateutil.parser import parse as date_parse
+from datetime import date
 from urllib.parse import urlparse
+
 
 # The extraction of the "important" features
 # Their importance was taken from:
 # https://github.com/VaibhavBichave/Phishing-URL-Detection
 class FeatureExtraction:
     features = []
-    def __init__(self,url):
+
+    def __init__(self, url):
         self.features = []
         self.url = url
         self.domain = ""
@@ -31,7 +29,7 @@ class FeatureExtraction:
 
         try:
             self.response = requests.get(url)
-            self.soup = BeautifulSoup(response.text, 'html.parser')
+            self.soup = BeautifulSoup(self.response.text, 'html.parser')
         except:
             pass
 
@@ -46,7 +44,7 @@ class FeatureExtraction:
         except:
             pass
 
-        # All the 14 featurs for the extended version
+        # All the 14 features for the extended version
         self.features.append(self.UsingIp())
         self.features.append(self.longUrl())
         self.features.append(self.prefixSuffix())
@@ -62,7 +60,7 @@ class FeatureExtraction:
         self.features.append(self.GoogleIndex())
         self.features.append(self.LinksPointingToPage())
 
-     # 1.UsingIp
+    # 1.UsingIp
     def UsingIp(self):
         try:
             ipaddress.ip_address(self.url)
@@ -74,12 +72,12 @@ class FeatureExtraction:
     def longUrl(self):
         if len(self.url) < 54:
             return 1
-        if len(self.url) >= 54 and len(self.url) <= 75:
+        if 54 <= len(self.url) <= 75:
             return 0
         return -1
 
     # 3.shortUrl
-    
+
     # 4.Symbol@
 
     # 5.Redirecting//
@@ -93,7 +91,7 @@ class FeatureExtraction:
             return 1
         except:
             return -1
-    
+
     # 7.SubDomains
     def SubDomains(self):
         dot_count = len(re.findall("\.", self.url))
@@ -119,61 +117,63 @@ class FeatureExtraction:
             expiration_date = self.whois_response.expiration_date
             creation_date = self.whois_response.creation_date
             try:
-                if(len(expiration_date)):
+                if len(expiration_date):
                     expiration_date = expiration_date[0]
             except:
                 pass
             try:
-                if(len(creation_date)):
+                if len(creation_date):
                     creation_date = creation_date[0]
             except:
                 pass
 
-            age = (expiration_date.year-creation_date.year)*12+ (expiration_date.month-creation_date.month)
-            if age >=12:
+            age = (expiration_date.year - creation_date.year) * 12 + (expiration_date.month - creation_date.month)
+            if age >= 12:
                 return 1
             return -1
         except:
             return -1
 
     # 10. Favicon
-    
+
     # 11. NonStdPort
 
     # 12. HTTPSDomainURL
-   
+
     # 13. RequestURL
     def RequestURL(self):
+        success = 0
+        i = 0
         try:
             for img in self.soup.find_all('img', src=True):
                 dots = [x.start(0) for x in re.finditer('\.', img['src'])]
                 if self.url in img['src'] or self.domain in img['src'] or len(dots) == 1:
                     success = success + 1
-                i = i+1
+                i = i + 1
 
             for audio in self.soup.find_all('audio', src=True):
                 dots = [x.start(0) for x in re.finditer('\.', audio['src'])]
                 if self.url in audio['src'] or self.domain in audio['src'] or len(dots) == 1:
                     success = success + 1
-                i = i+1
+                i = i + 1
 
             for embed in self.soup.find_all('embed', src=True):
                 dots = [x.start(0) for x in re.finditer('\.', embed['src'])]
                 if self.url in embed['src'] or self.domain in embed['src'] or len(dots) == 1:
                     success = success + 1
-                i = i+1
+                i = i + 1
 
             for iframe in self.soup.find_all('iframe', src=True):
                 dots = [x.start(0) for x in re.finditer('\.', iframe['src'])]
                 if self.url in iframe['src'] or self.domain in iframe['src'] or len(dots) == 1:
                     success = success + 1
-                i = i+1
+                i = i + 1
 
             try:
-                percentage = success/float(i) * 100
+                percentage = success / float(i) * 100
                 if percentage < 22.0:
                     return 1
-                elif((percentage >= 22.0) and (percentage < 61.0)):
+                elif (percentage >= 22.0) and (percentage < 61.0):
                     return 0
                 else:
                     return -1
@@ -181,13 +181,14 @@ class FeatureExtraction:
                 return 0
         except:
             return -1
-    
+
     # 14. AnchorURL
     def AnchorURL(self):
         try:
-            i,unsafe = 0,0
+            i, unsafe = 0, 0
             for a in self.soup.find_all('a', href=True):
-                if "#" in a['href'] or "javascript" in a['href'].lower() or "mailto" in a['href'].lower() or not (url in a['href'] or self.domain in a['href']):
+                if "#" in a['href'] or "javascript" in a['href'].lower() or "mailto" in a['href'].lower() or not (
+                        self.url in a['href'] or self.domain in a['href']):
                     unsafe = unsafe + 1
                 i = i + 1
 
@@ -195,7 +196,7 @@ class FeatureExtraction:
                 percentage = unsafe / float(i) * 100
                 if percentage < 31.0:
                     return 1
-                elif ((percentage >= 31.0) and (percentage < 67.0)):
+                elif (percentage >= 31.0) and (percentage < 67.0):
                     return 0
                 else:
                     return -1
@@ -208,25 +209,25 @@ class FeatureExtraction:
     # 15. LinksInScriptTags
     def LinksInScriptTags(self):
         try:
-            i,success = 0,0
-        
+            i, success = 0, 0
+
             for link in self.soup.find_all('link', href=True):
                 dots = [x.start(0) for x in re.finditer('\.', link['href'])]
                 if self.url in link['href'] or self.domain in link['href'] or len(dots) == 1:
                     success = success + 1
-                i = i+1
+                i = i + 1
 
             for script in self.soup.find_all('script', src=True):
                 dots = [x.start(0) for x in re.finditer('\.', script['src'])]
                 if self.url in script['src'] or self.domain in script['src'] or len(dots) == 1:
                     success = success + 1
-                i = i+1
+                i = i + 1
 
             try:
                 percentage = success / float(i) * 100
                 if percentage < 17.0:
                     return 1
-                elif((percentage >= 17.0) and (percentage < 81.0)):
+                elif (percentage >= 17.0) and (percentage < 81.0):
                     return 0
                 else:
                     return -1
@@ -238,9 +239,9 @@ class FeatureExtraction:
     # 16. ServerFormHandler
     def ServerFormHandler(self):
         try:
-            if len(self.soup.find_all('form', action=True))==0:
+            if len(self.soup.find_all('form', action=True)) == 0:
                 return 1
-            else :
+            else:
                 for form in self.soup.find_all('form', action=True):
                     if form['action'] == "" or form['action'] == "about:blank":
                         return -1
@@ -252,7 +253,7 @@ class FeatureExtraction:
             return -1
 
     # 17. InfoEmail
-    
+
     # 18. AbnormalURL
 
     # 19. WebsiteForwarding
@@ -262,24 +263,24 @@ class FeatureExtraction:
     # 21. DisableRightClick
 
     # 22. UsingPopupWindow
-    
+
     # 23. IframeRedirection
-    
+
     # 24. AgeofDomain
-    
+
     # 25. DNSRecording    
     def DNSRecording(self):
         try:
             creation_date = self.whois_response.creation_date
             try:
-                if(len(creation_date)):
+                if len(creation_date):
                     creation_date = creation_date[0]
             except:
                 pass
 
-            today  = date.today()
-            age = (today.year-creation_date.year)*12+(today.month-creation_date.month)
-            if age >=6:
+            today = date.today()
+            age = (today.year - creation_date.year) * 12 + (today.month - creation_date.month)
+            if age >= 6:
                 return 1
             return -1
         except:
@@ -288,11 +289,12 @@ class FeatureExtraction:
     # 26. WebsiteTraffic   
     def WebsiteTraffic(self):
         try:
-            rank = BeautifulSoup(urllib.request.urlopen("http://data.alexa.com/data?cli=10&dat=s&url=" + url).read(), "xml").find("REACH")['RANK']
-            if (int(rank) < 100000):
+            rank = BeautifulSoup(urllib.request.urlopen("http://data.alexa.com/data?cli=10&dat=s&url=" + url).read(),
+                                 "xml").find("REACH")['RANK']
+            if int(rank) < 100000:
                 return 1
             return 0
-        except :
+        except:
             return -1
 
     # 27. PageRank
@@ -326,12 +328,14 @@ class FeatureExtraction:
     def getFeaturesList(self):
         return self.features
 
+
 # The extraction of the 5 most "important" features
 # Their importance was taken from:
 # https://github.com/VaibhavBichave/Phishing-URL-Detection
 class ReducedFeatureExtraction:
     features = []
-    def __init__(self,url):
+
+    def __init__(self, url):
         self.features = []
         self.url = url
         self.domain = ""
@@ -341,7 +345,7 @@ class ReducedFeatureExtraction:
 
         try:
             self.response = requests.get(url)
-            self.soup = BeautifulSoup(response.text, 'html.parser')
+            self.soup = BeautifulSoup(self.response.text, 'html.parser')
         except:
             pass
 
@@ -366,7 +370,7 @@ class ReducedFeatureExtraction:
             return 1
         except:
             return -1
-    
+
     # 8.HTTPS
     def Hppts(self):
         try:
@@ -380,9 +384,10 @@ class ReducedFeatureExtraction:
     # 14. AnchorURL
     def AnchorURL(self):
         try:
-            i,unsafe = 0,0
+            i, unsafe = 0, 0
             for a in self.soup.find_all('a', href=True):
-                if "#" in a['href'] or "javascript" in a['href'].lower() or "mailto" in a['href'].lower() or not (url in a['href'] or self.domain in a['href']):
+                if "#" in a['href'] or "javascript" in a['href'].lower() or "mailto" in a['href'].lower() or not (
+                        self.url in a['href'] or self.domain in a['href']):
                     unsafe = unsafe + 1
                 i = i + 1
 
@@ -390,7 +395,7 @@ class ReducedFeatureExtraction:
                 percentage = unsafe / float(i) * 100
                 if percentage < 31.0:
                     return 1
-                elif ((percentage >= 31.0) and (percentage < 67.0)):
+                elif (percentage >= 31.0) and (percentage < 67.0):
                     return 0
                 else:
                     return -1
@@ -403,25 +408,25 @@ class ReducedFeatureExtraction:
     # 15. LinksInScriptTags
     def LinksInScriptTags(self):
         try:
-            i,success = 0,0
-        
+            i, success = 0, 0
+
             for link in self.soup.find_all('link', href=True):
                 dots = [x.start(0) for x in re.finditer('\.', link['href'])]
                 if self.url in link['href'] or self.domain in link['href'] or len(dots) == 1:
                     success = success + 1
-                i = i+1
+                i = i + 1
 
             for script in self.soup.find_all('script', src=True):
                 dots = [x.start(0) for x in re.finditer('\.', script['src'])]
                 if self.url in script['src'] or self.domain in script['src'] or len(dots) == 1:
                     success = success + 1
-                i = i+1
+                i = i + 1
 
             try:
                 percentage = success / float(i) * 100
                 if percentage < 17.0:
                     return 1
-                elif((percentage >= 17.0) and (percentage < 81.0)):
+                elif (percentage >= 17.0) and (percentage < 81.0):
                     return 0
                 else:
                     return -1
@@ -433,11 +438,12 @@ class ReducedFeatureExtraction:
     # 26. WebsiteTraffic   
     def WebsiteTraffic(self):
         try:
-            rank = BeautifulSoup(urllib.request.urlopen("http://data.alexa.com/data?cli=10&dat=s&url=" + url).read(), "xml").find("REACH")['RANK']
-            if (int(rank) < 100000):
+            rank = BeautifulSoup(urllib.request.urlopen("http://data.alexa.com/data?cli=10&dat=s&url=" + url).read(),
+                                 "xml").find("REACH")['RANK']
+            if int(rank) < 100000:
                 return 1
             return 0
-        except :
+        except:
             return -1
 
     def getFeaturesList(self):
