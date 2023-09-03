038cf4cf791e9abc5461500b477f83daf68d8d65
hilalewin <hilalewin98@gmail.com>
2023-09-03 16:30:33 +0300

    Reformated email.py

--- Email.py
+++ Email.py
@@ -1,13 +1,15 @@
 import json
 
+
 class Email:
-    def __init__(self, preferences, links, decoded_content, indicator_of_first_time_sender, indicator_of_first_time_domain):
+    def __init__(self, preferences, links, decoded_content, indicator_of_first_time_sender,
+                 indicator_of_first_time_domain):
         self.preferences = preferences
         self.links = links
-        self.decoded_content  =  decoded_content 
+        self.decoded_content = decoded_content
         self.indicator_of_first_time_sender = indicator_of_first_time_sender
-        self.indicator_of_first_time_domain = indicator_of_first_time_domain 
-        
+        self.indicator_of_first_time_domain = indicator_of_first_time_domain
+
     def to_json(self):
         return json.dumps(self.__dict__)
 
@@ -15,6 +17,6 @@ class Email:
     def from_json(cls, json_str):
         data = json.loads(json_str)
         return cls(**data)
-    
+
     def __str__(self):
-        return f"Sender Email: {self.sender_email}\nTime: {self.time}\nSubject: {self.subject}\nContent: {self.content}\nLinks: {self.links}\nDecoded Content: {self.decoded_content}\nCounter from Sender: {self.indicator_of_first_time_sender}\nCounter from Doamin: {self.indicator_of_first_time_domain}"
+        return f"Links: {self.links}\nDecoded Content: {self.decoded_content}\nCounter from Sender: {self.indicator_of_first_time_sender}\nCounter from Doamin: {self.indicator_of_first_time_domain}"
